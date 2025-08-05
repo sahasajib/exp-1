3a01e2341672e7232751f07f4f319ae0b8488d53
sahasajib <sajibsaha320@gmail.com>
2025-08-05 20:34:10 +0600

    first commit

---
+++ main.go
@@ -0,0 +1,96 @@
+package main
+
+import (
+	"encoding/json"
+	"log/slog"
+	"net/http"
+
+	"github.com/gorilla/mux/Desktop/go/exp-1/types"
+)
+
+var students[] types.Student
+
+func handleCors(w http.ResponseWriter){
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
+	w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
+	w.Header().Set("Content-Type", "application/json")
+}
+
+func handleOptions(w http.ResponseWriter, r *http.Request) {
+	if r.Method == http.MethodOptions {
+		w.WriteHeader(http.StatusOK)
+	}
+}
+
+func sendData(w http.ResponseWriter, data interface{}, statusCode int){
+	w.WriteHeader(statusCode)
+	encoder := json.NewEncoder(w) 
+	encoder.Encode(data)
+} 
+
+func getStudent(w http.ResponseWriter, r *http.Request){
+	handleCors(w)
+	handleOptions(w, r)
+	if r.Method != http.MethodGet {
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
+	if len(students) == 0 {
+		http.Error(w, "No students found", http.StatusNotFound)
+		return
+	}
+
+	sendData(w, students, http.StatusAccepted)
+}
+
+func createStudent(w http.ResponseWriter, r *http.Request) {
+	handleCors(w)
+	handleOptions(w, r)
+	if r.Method != http.MethodPost {
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
+
+	var student types.Type
+	decoder := json.NewDecoder(r.Body)
+	err := decoder.Decode(&student)
+	if err != nil {
+		http.Error(w, "Invalid request body", http.StatusBadRequest)
+		return
+	}
+
+	students = append(students, student)
+	sendData(w, student, http.StatusCreated)
+}
+
+func main() {
+	router := http.NewServeMux()
+	router.HandleFunc("/student", getStudent)
+	router.HandleFunc("/student/create", createStudent)
+
+	slog.Info("Server is running on port 8082")
+	if err := http.ListenAndServe(":8082", router); err != nil {
+		slog.Error("Failed to start server", "error", err)
+		return
+	}
+}
+
+func init(){
+	stu1 := types.Student{
+		Name:  "abc",
+		Phone: "01700000000",
+		Email: "abc@gmail.com",
+		Age:   25,
+	}
+
+	stu2 := types.Student{
+		Name:  "xyz",
+		Phone: "01800000000",
+		Email: "xyz@gmail.com",
+		Age:   30,
+	}
+
+	students = append(students, stu1)
+	students = append(students, stu2)
+}
---
+++ types/types.go
@@ -0,0 +1,8 @@
+package types
+
+type Student struct {
+	Name string  `json:"name"`
+	Phone string `json:"phone"`
+	Email string `json:"email"`
+	Age int `json:"age"`
+}
